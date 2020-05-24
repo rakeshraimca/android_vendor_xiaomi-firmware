914e8cc8ea8c3d952426e58c699e96ff5e9a28f7
sarveshrulz <sarveshkardekar@gmail.com>
2020-05-24 13:43:29 +0530

    firmware: initial commit

---
+++ firmware.mk
@@ -0,0 +1,20 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/phoenix/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi-firmware/phoenix/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi-firmware/phoenix/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi-firmware/phoenix/aop.mbn:install/firmware-update/aop.mbn \
+    vendor/xiaomi-firmware/phoenix/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi-firmware/phoenix/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi-firmware/phoenix/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi-firmware/phoenix/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi-firmware/phoenix/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi-firmware/phoenix/imagefv.elf:install/firmware-update/imagefv.elf \
+    vendor/xiaomi-firmware/phoenix/km4.mbn:install/firmware-update/km4.mbn \
+    vendor/xiaomi-firmware/phoenix/multi_image.mbn:install/firmware-update/multi_image.mbn \
+    vendor/xiaomi-firmware/phoenix/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    vendor/xiaomi-firmware/phoenix/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi-firmware/phoenix/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi-firmware/phoenix/uefi_sec.mbn:install/firmware-update/uefi_sec.mbn \
+    vendor/xiaomi-firmware/phoenix/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi-firmware/phoenix/xbl_config.elf:install/firmware-update/xbl_config.elf
